e3241286be9e273dcfa4f1fe74cbd7f7f26fbb0b
Brienyll <Brienyll29@gmail.com>
2019-07-02 00:41:29 -0700

    added constant folders

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+import { FaRProject } from "react-icons/fa"
+const Navbar = () => {
+  return (
+    <div>
+      <h1>i'm navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
