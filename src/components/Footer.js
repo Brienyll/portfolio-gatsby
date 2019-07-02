ebfebc49b8c269ebb88be828309f8a7b5542b8a4
Brienyll <Brienyll29@gmail.com>
2019-07-02 14:01:51 -0700

    fixed path

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
     <div className={styles.links}>
     {links.map((item, index) => {
       return (
-      <Link key={index} to={item.mpat}>
+      <Link key={index} to={item.path}>
         {item.text}
       </Link>
       )
