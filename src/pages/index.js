fc70f9ce2267faf54f17144b19c0423c5038a990
Brienyll <Brienyll29@gmail.com>
2019-07-03 02:18:39 -0700

    added banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
 import React from "react"
-//import { Link } from "gatsby"
+import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
 export default () => (
   <Layout>
-    <SimpleHero />
+    <SimpleHero>
+      <Banner title="Brix Angeles" info="A web developer from Los Angeles. A Student, self-taught, and Grow with Google Front End Scholar.">
+        <Link to="/about" className="btn-white">
+          About Me
+        </Link>
+      </Banner>
+    </SimpleHero>
   </Layout>
 )
