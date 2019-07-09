328318dbbc018e5c6e2a8e589ce564d3f0d06129
Brienyll <Brienyll29@gmail.com>
2019-07-09 03:14:30 -0700

    added projects

--- src/components/Home/Projects.js
+++ src/components/Home/Projects.js
@@ -2,17 +2,16 @@ import React from 'react'
 import Title from '../Title'
 import styles from "../../css/projects.module.css"
 import projects from '../../constants/projects'
-import P1 from "../../images/scfacer29.png"
 
 const Projects = () => {
   return (
     <section className={styles.projects}>
       <Title title="Projects" />
-      <div classname={styles.center}>
+      <div className={styles.center}>
         {
           projects.map((item, index)=> {
             return <article key={index} className={styles.project}>
-            <img src={P1}/>
+            <span>{item.image}</span>
             <h4>{ item.title}</h4>
             <p>{item.text}</p>
             </article>
--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,6 +1,22 @@
+import React from "react"
+import P1 from "../images/scfacer29.png"
+import P2 from "../images/scbooks29.png"
+import P3 from "../images/scfriends29.png"
+
 export default [
   {
+    image: <img src={P1} alt="Face Recognition App" />,
     title: "Face Recognition App",
     text: "Built a web Application that detects and identifies images that you upload from the web using machine learning and neural networks with the help of clarifai's API."
+  },
+  {
+    image: <img src={P2} alt="Virtual Bookshelf" />,
+    title: "Virtual Bookshelf",
+    text: "Built a responsive web application that allows users to select and categorize books into a virtual bookshelf. Leveraged React to support a dynamic user interface that interacts with an API server and client library."
+  },
+  {
+    image: <img src={P3} alt="My Friends" />,
+    title: "My Friends",
+    text: "Single Page Application using React that searches your robot friends asyncronously. Leveraged React for dynamic state management."
   }
 ]
