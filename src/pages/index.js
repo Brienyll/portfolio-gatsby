3bfe671d0f39bd4daf3e52f95c341374b8f10bfa
Brienyll <Brienyll29@gmail.com>
2019-07-22 03:19:15 -0700

    added styledhero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
-import SimpleHero from "../components/SimpleHero"
 import Banner from "../components/Banner"
 import About from "../components/Home/About"
 import Projects from "../components/Home/Projects"
+import StyledHero from "../components/StyledHero"
 
 export default () => (
   <Layout>
-    <SimpleHero>
-      <Banner title="Brix Angeles" info="A web developer from Los Angeles. A Student, self-taught, and Grow with Google Front End Scholar.">
+    <StyledHero home="true">
+    <Banner title="Brix Angeles" info="A web developer from Los Angeles. A Student, self-taught, and Grow with Google Front End Scholar.">
         <Link to="/about" className="btn-white">
           About Me
         </Link>
-      </Banner>
-    </SimpleHero>
+    </Banner>
+    </StyledHero>
+    
     <About />
     <Projects />
   </Layout>
